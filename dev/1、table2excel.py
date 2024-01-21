0b5275d74884c4686191b304d6b24c15181890f8
CoderWanFeng <1957875073@qq.com>
2024-01-22 00:04:06 +0800

    update README.md

--- dev/1、table2excel.py
+++ dev/1、table2excel.py
@@ -1,8 +1,8 @@
 # -*- coding: UTF-8 -*-
 '''
-@作者  ：B站/抖音/微博/小红书/公众号，都叫：程序员晚枫
-@微信     ：CoderWanFeng : https://mp.weixin.qq.com/s/B1V6KeXc7IOEB8DgXLWv3g
-@个人网站      ：www.python-office.com
+@作者  ：B站/抖音/微博/小红书/公众号，都叫：程序员晚枫，微信：CoderWanFeng
+@读者群     ：http://www.python4office.cn/wechat-group/
+@学习网站      ：www.python-office.com
 @代码日期    ：2023/7/13 22:08 
 @本段代码的视频说明     ：
 '''
